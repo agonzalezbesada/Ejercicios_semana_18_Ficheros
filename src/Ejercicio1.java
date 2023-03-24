c29351597565c00ec7fd312e57e83cc24d1abc91
agonzalezbesada <agonzalezbesada@danielcastelao.org>
2023-03-24 22:30:20 +0100

    Ejercicio1 3.0

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -6,16 +6,16 @@ public class Ejercicio1 {
     }
 
     public boolean escribir(String texto, String ruta) {
-        boolean check = false;
+        boolean check = false; // Señal que comprueba el proceso
         FileWriter escritura;
         try {
             escritura = new FileWriter(ruta);
             escritura.write(texto);
             escritura.close();
-            check = true;
+            check = true; // Señal de éxito
         } catch (IOException e) {
             System.out.println(e.toString());
-            check = false;
+            check = false; // Señal de error
         }
         return check;
     }
