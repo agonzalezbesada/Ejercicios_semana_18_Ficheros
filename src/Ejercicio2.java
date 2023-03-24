2b33be4c98bcfbacc6fb6dee7f96a5559ad2d8b7
agonzalezbesada <agonzalezbesada@danielcastelao.org>
2023-03-24 22:30:35 +0100

    Ejercicio2 2.0

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -5,23 +5,30 @@ public class Ejercicio2 {
 
     }
 
-    public boolean leer(String ruta) {
-        boolean check = false;
+    public StringBuffer leer(String ruta) {
+        boolean check = false; // Señal que comprueba el proceso
+        // Creo una cadena dinámica con StringBuffer
+        StringBuffer cadena = new StringBuffer();
+        int indice = -1; // Creo un punto de inicio para el índice del StringBuffer
         FileReader lectura;
         try {
             lectura = new FileReader(ruta);
             int caracter = lectura.read();
             while (caracter != -1) {
+                indice ++; // Igualo el indice a 0 en la primera vuelta, y en las siguientes aumenta
                 char imprimir = (char) caracter;
-                System.out.println(imprimir);
+                cadena.insert(indice,imprimir); // Inserto los char de uno en uno
                 caracter = lectura.read();
             }
             lectura.close();
-            check = true;
+            check = true; // Señal de éxito
         } catch (IOException e) {
             e.printStackTrace();
-            check = false;
+            cadena.setLength(0); // Vacía el StringBuffer
+            cadena.append(e).toString(); // Iguala el StringBuffer a la cadena de error para ser devuelta
+
+            check = false; // Señal de error
         }
-        return check;
+        return cadena;
     }
 }
